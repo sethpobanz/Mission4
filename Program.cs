cb7e076ef340d6db750481b6a84fcb76d11fb98f
sethpobanz <sethpobanz@gmail.com>
2024-01-30 16:33:52 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mission4;
+
+tictactoetools ttt = new tictactoetools();
+
+Console.WriteLine("Welcome to our game of tic-tac-toe!");
+//• Create a game board array to store the players’ choices
+
+//• Ask each player in turn for their choice and update the game board array
+
+//• Print the board by calling the method in the supporting class
+
+//• Check for a winner by calling the method in the supporting class, and notify the players
+//when a win has occurred and which player won the game
+
+
---
+++ tictactoetools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission4
+{
+    internal class tictactoetools
+    {
+        // Receive the Game Board Array
+
+        // Method 1: Print the board based on the info passed to it
+
+        // Method 2: a method that receives the game board array as input and returns if there is a
+        // winner and who it was
+
+
+
+
+    }
+}
